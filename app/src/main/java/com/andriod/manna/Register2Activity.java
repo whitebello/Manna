8d9e093fa6df6562d18e781032671d089d637c48
쫑환 <whitebello@gmail.com>
2020-09-26 22:39:37 +0900

    로그인/회원가입화면작업 프래그먼트 정보화면

--- app/src/main/java/com/andriod/manna/Register2Activity.java
+++ app/src/main/java/com/andriod/manna/Register2Activity.java
@@ -2,7 +2,15 @@ package com.andriod.manna;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class Register2Activity extends AppCompatActivity {
 
@@ -10,5 +18,18 @@ public class Register2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
+
+        Button btn_reg_fin;
+        btn_reg_fin = findViewById(R.id.btn_reg_fin);
+
+        btn_reg_fin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
